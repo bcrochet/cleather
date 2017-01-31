3cc5c28cf973acb90ca0beec70772c747d07a6ae
Brad P. Crochet <brad@redhat.com>
2017-01-31 07:26:46 -0500

    Reorganize files and add tox

---
+++ setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+    name='cleather',
+    version='0.0.1',
+    description='Python CLI Weather App',
+    url='https://github.com/cswiii/cleather',
+    author='Corey Welton',
+    author_email='cswiii@gmail.com',
+    license='GPL',  # Right?
+    packages=['cleather'],
+    zip_safe=False
+)
